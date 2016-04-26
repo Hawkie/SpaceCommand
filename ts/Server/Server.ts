ae11975b88b1d9baa330ca0db87d5d7905cdabd6
icedvariables <macodeblacko@gmail.com>
2016-04-26 11:42:29 +0100

    Basic server skeleton

---
+++ ts/Server/Server.ts
@@ -0,0 +1,10 @@
+import WebSocket = require("ws");
+
+var port : number = 13579;
+var server = new WebSocket.Server({port: port});
+
+server.on("connection", ws => {
+    console.log("Connection: " + ws);
+});
+
+console.log("Server is listening on port: " + port);
