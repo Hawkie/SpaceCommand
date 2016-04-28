c47d484c7538b2122ea921ca7ca3a9066cececea
icedvariables <macodeblacko@gmail.com>
2016-04-28 19:31:21 +0100

    Removed server build files

--- server/build/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var WebSocket = require('ws');
-var port = 13579;
-var server = new WebSocket.Server({ port: port });
-server.on("connection", function (ws) {
-    console.log("Connection: " + ws);
-});
-console.log("Server is listening on port: " + port);
