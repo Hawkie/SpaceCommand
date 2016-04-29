719e9f67a8b5db7d00982e445965a795d4826d2b
icedvariables <macodeblacko@gmail.com>
2016-04-29 16:13:48 +0100

    Reverted changes made in last two commits

--- ts/Buildings/Building.ts
+++
@@ -1,6 +0,0 @@
-export interface IBuilding{
-    cost : number;              // Price to build.
-    powerAffect : number;       // How much power it provides or, if negative, how much it draws.
-    workersAffect : number;     // How many workers this building adds or removes.
-    drawResources(resources);   // Allow the building to modify how many resources the plant has (eg. A coal plant would need to remove coal).
-}
