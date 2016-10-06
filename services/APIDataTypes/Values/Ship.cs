c56d8c0d7a3509f2e36f9dbb94d13e176afa997a
Hawkie <paulhawkins123@gmail.com>
2016-10-06 15:35:10 +0100

    Add datatypes

---
+++ services/APIDataTypes/Values/Ship.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIDataTypes.Values
+{
+    public class Ship
+    {
+        public string PlayerName { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int VelX { get; set; }
+        public int VelY { get; set; }
+
+        public Ship(string PlayerName, int x, int y, int velX, int velY)
+        {
+            VelY = velY;
+            VelX = velX;
+            Y = y;
+            X = x;
+            this.PlayerName = PlayerName;
+        }
+    }
+}
